86dccf5d664048e8be147760bef6229f7e547239
Derin <160138070+deriiinjv@users.noreply.github.com>
2026-03-27 23:11:58 +0530

    Update image path for visualization

--- visualize.py
+++ visualize.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 from inference import process_image
 
-img_path = "processed_data/val/good/sample.jpg"
+img_path = "processed_data/val/good/-_-_2007_14-_H180_V0_JPG.rf.ea1c6c9e25d058a0f8a0b90257c80c10.jpg.jpg"
 
 img = cv2.imread(img_path)
 
